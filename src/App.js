d5c351bd274f6a85557ae2bd3e0d55cad770d936
Anh Hoang Vo <vohoanganhkhtn@gmail.com>
2017-11-21 02:08:46 +0200

    update the list of participants

--- src/App.js
+++ src/App.js
@@ -15,27 +15,28 @@ class App extends Component {
         phone: '',
         isEditing: false
       },
-      guests: [
-        {
-          name: 'Treasure',
-          email: 'Treasure@gmail.com',
-          phone: '0469651192',
-          isEditing: false
-        },
-
-        {
-          name: 'John',
-          email: 'John@gmail.com',
-          phone: '0469648342',
-          isEditing: false
-        },
-        {
-          name: 'Nick',
-          email: 'nick@gmail.com',
-          phone: '0469551192',
-          isEditing: false
-        }
-      ]
+      guests:
+        [{"name":"Sisely Josefer","email":"sjosefer0@comsenz.com","phone":"446-428-4124"},
+{"name":"Rosaleen Husselbee","email":"rhusselbee1@indiegogo.com","phone":"649-813-5007"},
+{"name":"Gilberta Capner","email":"gcapner2@globo.com","phone":"455-592-6870"},
+{"name":"Kerry Dales","email":"kdales3@simplemachines.org","phone":"574-207-0912"},
+{"name":"Clarisse Smullin","email":"csmullin4@elpais.com","phone":"407-266-0391"},
+{"name":"Candie Gunney","email":"cgunney5@weebly.com","phone":"856-351-9364"},
+{"name":"Darin Sangar","email":"dsangar6@springer.com","phone":"305-274-2871"},
+{"name":"Juanita Townsend","email":"jtownsend7@tmall.com","phone":"916-421-0245"},
+{"name":"Ruthi Mankor","email":"rmankor8@elpais.com","phone":"451-823-0465"},
+{"name":"Burr Apedaile","email":"bapedaile9@topsy.com","phone":"205-535-4588"},
+{"name":"Caleb Nehl","email":"cnehla@weather.com","phone":"524-910-4630"},
+{"name":"Guillermo Wither","email":"gwitherb@arstechnica.com","phone":"868-712-4496"},
+{"name":"Chase Giral","email":"cgiralc@ca.gov","phone":"732-399-3083"},
+{"name":"Del Halms","email":"dhalmsd@simplemachines.org","phone":"377-748-0808"},
+{"name":"Dov Curmi","email":"dcurmie@cdbaby.com","phone":"911-476-1891"},
+{"name":"Erinn Freebury","email":"efreeburyf@google.co.uk","phone":"864-883-6437"},
+{"name":"Rudie Dronsfield","email":"rdronsfieldg@tinyurl.com","phone":"809-628-6926"},
+{"name":"Justine Ofer","email":"joferh@list-manage.com","phone":"272-959-9065"},
+{"name":"Barclay Sothcott","email":"bsothcotti@desdev.cn","phone":"812-694-3149"},
+{"name":"Ryley Abramin","email":"rabraminj@patch.com","phone":"597-464-7030"}]
+      
     }
     // preserve the initial state in a new object
     this.baseState = this.state
@@ -116,15 +117,15 @@ class App extends Component {
 
     cancleAt = (indexToChange) => {
 
-      this.setState({
-        guests: this.state.guests.map((guest, index)=>{
-          if (index === indexToChange) {
-            console.log(this.baseState.guests[indexToChange]);
-            return this.baseState.guests[indexToChange]
-          }
-          return guest;
-        })
-      })
+      // this.setState({
+      //   guests: this.state.guests.map((guest, index)=>{
+      //     if (index === indexToChange) {
+      //       console.log(this.baseState.guests[indexToChange]);
+      //       return this.baseState.guests[indexToChange]
+      //     }
+      //     return guest;
+      //   })
+      // })
       this.toogleGuestPropertyAt('isEditing', indexToChange);
     }
 
